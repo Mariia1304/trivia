9b754e2220f484f84ad82d4df249a240e3284b09
Mariia <maria.bookvoed@gmail.com>
2019-03-26 10:03:23 -0300

    cambios en tercera categoria

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ function playGame() {
     document.getElementById("welcome").style.display = "none";
     document.getElementById('selectGame').innerHTML = "<p>Hola " + name + ' a continuación selecciona un tema de juego:</p><button onclick="playMovies()">Jugar con Películas</button><button onclick="playSeries()">Jugar con Series</button><button onclick="playDibujos()">Jugar con Dibujos</button>';
 }
-
+// categoría peliculas
 function playMovies() {
     setCounterZero();
     document.getElementById("selectGame").style.display = "none";
     document.getElementById('mainContainer').innerHTML = '<img alt="Dan" src="https://i.ebayimg.com/images/g/e~8AAOSwJo1bVb5W/s-l300.jpg" ><p>Pregunta 1: ¿Cuál es el nombre del teniente de Forrest Gump?</p>  <button onclick="questionOneMovies(\'Ryan\')">A: Ryan</button> </br></br> <button onclick="questionOneMovies(\'Sammy\')">B: Sammy</button> </br></br> <button onclick="questionOneMovies(\'Dan\')">C: Dan</button>';
 }
-
+//preguntas peliculas
 function questionOneMovies(answer) {
     if (answer === "Dan") {
         ++countCorrect;
@@ -49,13 +49,13 @@ function questionThreeMovies(answer) {
     }
     finalResults();
 }
-
+//Categpría Series
 function playSeries() {
     setCounterZero();
     document.getElementById("selectGame").style.display = "none";
     document.getElementById('mainContainer').innerHTML = '<img alt="Walter White" src="http://images6.fanpop.com/image/photos/37200000/Walter-White-breaking-bad-37267744-200-200.jpg" ><p>Pregunta 1: ¿Cuál es la profesión de Walter White en Breaking Bad?</p>  <button onclick="questionOneSeries(\'Bombero\')">A: Bombero</button> </br></br> <button onclick="questionOneSeries(\'Profesor\')">B: Profesor</button> </br></br> <button onclick="questionOneSeries(\'Policia\')">C: Policia</button>';
 }
-
+//preguntas series
 function questionOneSeries(answer) {
     if (answer === "Profesor") {
         ++countCorrect;
@@ -86,20 +86,20 @@ function questionThreeSeries(answer) {
 function playDibujos() {
     setCounterZero();
     document.getElementById("selectGame").style.display = "none";
-    document.getElementById('mainContainer').innerHTML = '<img alt="Walter White" src="http://images6.fanpop.com/image/photos/37200000/Walter-White-breaking-bad-37267744-200-200.jpg" ><p>Pregunta 1: ¿Cuál es la profesión de Walter White en Breaking Bad?</p>  <button onclick="questionOneSeries(\'Bombero\')">A: Bombero</button> </br></br> <button onclick="questionOneSeries(\'Profesor\')">B: Profesor</button> </br></br> <button onclick="questionOneSeries(\'Policia\')">C: Policia</button>';
+    document.getElementById('mainContainer').innerHTML = '<img alt="hijo de mufasa" src="http://images6.fanpop.com/image/photos/37700000/The-Lion-King-Simba-walt-disney-characters-37730625-200-200.png" ><p>Pregunta 1: ¿Cuál es el nombre de hijo de Mufasa?</p>  <button onclick="questionOneDibujos(\'Nala\')">A: Nala</button> </br></br> <button onclick="questionOneDibujos(\'Emilio\')">B: Emilio</button> </br></br> <button onclick="questionOneDibujos(\'Simba\')">C: Simba</button>';
 }
-//preguntas
+//preguntas dibujos
 function questionOneDibujos(answer) {
-    if (answer === "Profesor") {
+    if (answer === "Simba") {
         ++countCorrect;
     } else {
         ++countWrong;
     }
-    document.getElementById('mainContainer').innerHTML = '<img alt="Ragnar Lodbrok" src="https://i.pinimg.com/originals/bf/9a/f8/bf9af85da5f21e2ac0025f49c8110324.jpg" ><p>Pregunta 2: ¿Cuántos hijos varones tuvo Ragnar Lodbrok en la serie Vikings?</p>  <button onclick="questionTwoSeries(5)">A: 5</button> </br></br> <button onclick="questionTwoSeries(2)">B: 2</button> </br></br> <button onclick="questionTwoSeries(7)">C: 7</button>';
+    document.getElementById('mainContainer').innerHTML = '<img alt="Frozen" src="http://images6.fanpop.com/image/photos/35900000/Elsa-the-Snow-Queen-Icons-frozen-35962805-200-200.jpg" ><p>Pregunta 2: ¿Cuál es el poder que posee Elza? </p>  <button onclick="questionTwoDibujos(\'congelar cosas\')">Congelar cosas</button> </br></br> <button onclick="questionTwoDibujos(\'Derretir cosas\')">Derretir cosas</button> </br></br> <button onclick="questionTwoDibujos(\'Volar\')">Volar</button>';
 }
 
 function questionTwoDibujos(answer) {
-    if (answer === 5) {
+    if (answer === "congelar cosas") {
         ++countCorrect;
     } else {
         ++countWrong;
@@ -115,7 +115,7 @@ function questionThreeDibujos(answer) {
     }
     finalResults();
 }
-
+// result
 function finalResults() {
-    document.getElementById('mainContainer').innerHTML = '<p>¡Felicitaciones has terminado la trivia!</p> <span>Respuestas correctas: ' + countCorrect + '</span> </br><span>Respuestas incorrectas: ' + countWrong + '</span> </br></br><button onclick="playMovies()">Jugar con Películas</button><button onclick="playSeries()">Jugar con Series</button>';
+    document.getElementById('mainContainer').innerHTML = '<p>¡Felicitaciones has terminado la trivia!</p> <span>Respuestas correctas: ' + countCorrect + '</span> </br><span>Respuestas incorrectas: ' + countWrong + '</span> </br></br><button onclick="playMovies()">Jugar con Películas</button><button onclick="playSeries()">Jugar con Series</button><button onclick="playDibujos()">Jugar con Dibujos</button>';
 }
